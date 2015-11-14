48c9718a631ecaf7efa736bd953ca69c54b8d400
Nick Janssen <info@nickjanssen.com>
2015-11-14 17:55:10 +0900

    Initial commit, setup webpack + react

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './main.jsx',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ],
+
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
